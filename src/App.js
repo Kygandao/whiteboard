2328d0e911bfc177b7a262e40978d5b4aa1a70c3
Kygan <kygandao23@gmail.com>
2021-11-24 18:21:37 -0800

    troubleshooting drawing area size

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Whiteboard from './components/Whiteboard';
 function App() {
   return (
     <div className='flex items-center justify-center w-screen h-screen'>
-      <div className='w-5/6 h-5/6 bg-center bg-contain bg-whiteboard bg-no-repeat'>
+      <div className='w-5/6 h-5/6'>
         <Whiteboard/>
       </div>
     </div>
--- src/components/Whiteboard.js
+++ src/components/Whiteboard.js
@@ -1,10 +1,69 @@
-import React from 'react'
-import ColorPicker from './ColorPicker'
+import React, { useEffect, useRef, useState } from 'react'
+//import { useWhiteboard } from './WhiteboardContext'
+import blackmarker from '../images/blackmarker.png'
+import bluemarker from '../images/bluemarker.png'
+import greenmarker from '../images/greenmarker.png'
+import redmarker from '../images/redmarker.png'
+import eraser from '../images/eraser.jpg'
 
 const Whiteboard = () => {
+    const whiteboardRef = useRef(null)
+    const contextRef = useRef(null)
+    const [drawing, setDrawing] = useState(false)
+
+    useEffect(() => {
+        const whiteboard = whiteboardRef.current;
+        whiteboard.width = window.innerWidth * 2;
+        whiteboard.style.width = `${window.innerWidth}px`;
+        whiteboard.height = window.innerHeight * 2;
+        whiteboard.style.height = `${window.innerHeight}px`;
+    
+        const context = whiteboard.getContext('2d');
+        context.scale(2,2);
+        context.lineCap = 'round';
+        context.strokeStyle = 'black'
+        context.lineWidth = 4
+        contextRef.current = context;
+      }, [])
+    
+      const startDrawing =({nativeEvent}) => {
+        const {offsetX, offsetY } = nativeEvent;
+        contextRef.current.beginPath();
+        contextRef.current.moveTo(offsetX, offsetY);
+        setDrawing(true)
+      }
+    
+      const stopDrawing = () => {
+        contextRef.current.closePath();
+        setDrawing(false)
+      }
+    
+      const draw = ({nativeEvent}) => {
+        if (!drawing) {
+          return
+        } else {
+        const {offsetX, offsetY } = nativeEvent;
+        contextRef.current.lineTo(offsetX, offsetY);
+        contextRef.current.stroke()
+        }
+      }
+
     return (
-        <div className='flex items-end justify-end w-full h-full'>
-            <ColorPicker/>
+        <div className='flex flex-col items-center justify-center w-full h-full border-4 border-double border-black rounded-lg'>
+            <canvas className='w-11/12 h-5/6'
+                onMouseDown={startDrawing}
+                onMouseUp={stopDrawing}
+                onMouseMove={draw}
+                ref={whiteboardRef}   
+            />
+
+            <div className='flex items-end justify-center mb-2'>
+                <img src={blackmarker} alt='Black Marker' className='w-1/6 mx-3'/>
+                <img src={bluemarker} alt='Black Marker' className='w-1/6 mx-3'/>
+                <img src={greenmarker} alt='Black Marker' className='w-1/6 mx-3'/>
+                <img src={redmarker} alt='Black Marker' className='w-1/6 mx-3'/>
+                <img src={eraser} alt='Black Marker' className='w-1/12 mx-3'/>
+            </div>
         </div>
     )
 }
